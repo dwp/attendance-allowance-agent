2203e9d83809563faf9af1be48f8106f245b63e4
Matthew Barker <matthew.barker3@engineering.digital.dwp.gov.uk>
2025-08-13 09:14:19 +0100

    verify section added
    GitOrigin-RevId: 5dee4ceefaad982600f0d4e1414ec69f4d5a5f80

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,20 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+router.post('/iteration-1/change-details-answer', function (req, res) {
+
+    console.log(req.session.data)
+  var changeDetails = req.session.data['changeClaimDetails']
+
+  if (changeDetails == "name"){
+    res.redirect('/iteration-1/update/name')
+  } else if (changeDetails == "address") {
+    res.redirect('/iteration-1/update/address')
+  } else if (changeDetails == "postcode") {
+    res.redirect('/iteration-1/update/postcode')
+  } else {
+    res.redirect('/iteration-1/update/contact')
+  }
+
+})
