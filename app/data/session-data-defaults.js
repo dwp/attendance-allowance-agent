0f344af889cb8a78193f37fbb2c61372025f4349
Matthew Barker <matthew.barker3@engineering.digital.dwp.gov.uk>
2025-08-13 15:18:58 +0100

    json data linked with verify
    GitOrigin-RevId: c9b012b8625d3c4f5fe2348f98c1615d7c840ac6

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,5 +1,145 @@
 module.exports = {
 
-  // Insert values here
+  "claim" : {
+    "claimDates" : {
+      "submittedOn" : "13/08/2025"
+    },
+    "claimOverview" : {
+      "nationalInsurance" : "AB123456A",
+      "dateOfBirth" : {
+        "dd" : "10",
+        "mm" : "10",
+        "yyyy" : "1930"
+      },
+      "ageAtSubmission" : "94",
+      "fullName" : "Jon Smith",
+      "address" : {
+        "address1" : "123 test street",
+        "address2" : "",
+        "address3" : "test town",
+        "address4" : "",
+        "postcode" : "NE11AB"
+      },
+      "mainContactNumber" : "07811111111",
+      "whoIsApplying" : "I am applying for myself",
+      "nationality" : "British",
+      "otherBenefits" : [ 
+        "None"
+      ],
+      "specialRules" : "Yes",
+      "alternativeFormats" : "None"
+    },
+    "aboutYou" : {
+      "whoIsApplying" : "I am applying for myself",
+      "dateOfBirth" : {
+        "dd" : "10",
+        "mm" : "10",
+        "yyyy" : "1930"
+      },
+      "otherBenefits" : [ 
+        "None of these benefits"
+      ],
+      "nationalInsurance" : "AB123456A",
+      "firstName" : "Jon",
+      "lastName" : "Smith",
+      "address" : {
+        "address1" : "123 test street",
+        "address2" : "",
+        "address3" : "test town",
+        "address4" : "",
+        "postcode" : "NE11AB"
+      }
+    },
+    "claimInfo" : {
+      "nationality" : "British",
+      "whereDoYouLive" : "England",
+      "haveYouMovedFromScotland" : "No",
+      "outsideUKDuration" : "No",
+      "countryOutsideUKBenefits" : "No",
+      "countryOutsideUKWorking" : "No",
+      "careHomeOrHospital" : "Hospital",
+      "hospitalAddressName" : "sample hospital",
+      "hospitalAddressTownOrCity" : "sample town",
+      "hospitalAdmissionDate" : {
+        "dd" : "10",
+        "mm" : "06",
+        "yyyy" : "2025"
+      },
+      "hospitalFunding" : "Yes"
+    },
+    "specialRules" : {
+      "declaration" : "Yes",
+      "completed" : "No"
+    },
+    "healthInfo" : {
+      "list" : [ 
+        "C70 - Bone cancer"
+      ],
+      "listDates" : {
+        "condition1" : "03/21"
+      },
+      "specialRules" : "Yes",
+      "treatment" : "Yes",
+      "treatmentInfo" : "operation to replace my hip on 17 December 2022"
+    },
+    "consent" : {
+      "treatmentConsent" : "Yes, information about my health can be shared",
+      "gpCentreName" : "test surgery",
+      "gpName" : "mr GP",
+      "gpTelephone" : "01912222222",
+      "healthcareProfessionalOptions" : "Consultant",
+      "specialistName" : "mr consultant",
+      "specialistTelephone" : "01914444444"
+    },
+    "contactDetails" : {
+      "mainContactNumber" : "07811111111",
+      "addContactDetails" : "No",
+      "alternativeFormats" : "None, these do not apply to me",
+      "emailWanted" : "No"
+    },
+    "declaration" : {
+      "statement" : "Accepted and submitted on 13/08/2025"
+    }
+  },
+    "claimant" : {
+    "citizen_id": "11111111",
+    "citizen_guid": "5182b4581f98bd49a5f1c939d26c1b8892d1e82309a298f082cda2d610b0237c",
+    "nino": "RT000001A",
+    "first_name": "Jon",
+    "middle_name": "Michael",
+    "last_name": "Smith",
+    "date_of_birth": "2025-08-13",
+    "date_of_death": "2025-08-13",
+    "citizen_meta": {
+      "death_indicated": true,
+      "death_confirmed": true,
+      "special_rules_indicated": true,
+      "special_rules_confirmed": true,
+      "special_customer_record": true,
+      "identity_at_risk": true,
+      "communication_preferences": {
+        "welsh_speak": true,
+        "welsh_write": true,
+        "large_print": true,
+        "audio": true,
+        "braille": true,
+        "text_phone": true,
+        "relay_uk": true
+      }
+    },
+    "residency": [
+      {
+        "address_type": "residential",
+        "address_line1": "Test Line 1",
+        "address_line2": "Test Line 2",
+        "address_line3": "Test Line 3",
+        "address_line4": "Test Line 4",
+        "postcode": "NE1 1EN",
+        "country": "England",
+        "effective_from_date": "2025-08-13",
+        "effective_to_date": "2025-08-13"
+      }
+    ]
+  }
 
 }
--- app/routes.js
+++ app/routes.js
@@ -8,19 +8,33 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
 
+router.post('/iteration-1/verify-claimant-details-answer', function (req, res) {
+
+    var verifyAction = req.session.data['verifyAction']
+
+    if (verifyAction == "change"){
+        res.redirect('/iteration-1/change-claimant-details')
+    } else if (verifyAction == "close") {
+        res.redirect('/iteration-1/close-claim')
+    } else {
+        req.session.data['taskVerifyStatus'] = "complete"
+        res.redirect('/iteration-1/claim-details')
+    }
+
+})
+
 router.post('/iteration-1/change-details-answer', function (req, res) {
 
-    console.log(req.session.data)
-  var changeDetails = req.session.data['changeClaimDetails']
+    var changeDetails = req.session.data['changeClaimDetails']
 
-  if (changeDetails == "name"){
-    res.redirect('/iteration-1/update/name')
-  } else if (changeDetails == "address") {
-    res.redirect('/iteration-1/update/address')
-  } else if (changeDetails == "postcode") {
-    res.redirect('/iteration-1/update/postcode')
-  } else {
-    res.redirect('/iteration-1/update/contact')
-  }
+    if (changeDetails == "name"){
+        res.redirect('/iteration-1/update/name')
+    } else if (changeDetails == "address") {
+        res.redirect('/iteration-1/update/address')
+    } else if (changeDetails == "postcode") {
+        res.redirect('/iteration-1/update/postcode')
+    } else {
+        res.redirect('/iteration-1/update/contact')
+    }
 
 })
