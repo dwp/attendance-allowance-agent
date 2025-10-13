bc82d6afd2e03a59230473a5f938b984e7962a03
Matthew Barker <matthew.barker3@engineering.digital.dwp.gov.uk>
2025-10-13 12:41:41 +0100

    v1 register added
    GitOrigin-RevId: decb90520d5667e948bd693e222de61a5859b463

--- app/routes.js
+++ app/routes.js
@@ -8,37 +8,90 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
 
-router.post('/iteration-1/verify-claimant-details-answer', function (req, res) {
+// iteration 1
 
-    var verifyAction = req.session.data['verifyAction']
+router.post('/iteration-1/search-nino-answer', function (req, res) {
 
-    if (verifyAction == "change"){
-        res.redirect('/iteration-1/change-claimant-details')
-    } else if (verifyAction == "close") {
-        res.redirect('/iteration-1/close-claim')
+    var nino = req.session.data['searchNino']
+
+    if (nino == "QQ123456C"){
+        res.redirect('/iteration-1/claimant-result')
     } else {
-        req.session.data['taskVerifyStatus'] = "complete"
-        res.redirect('/iteration-1/claim')
+        res.redirect('/iteration-1/claimant-details')
     }
 
 })
 
-router.post('/iteration-1/change-details-answer', function (req, res) {
+router.post('/iteration-1/decision-answer', function (req, res) {
+
+    var decision = req.session.data['decisionAction']
+
+    if (decision == "Disallowed"){
+        res.redirect('/iteration-1/exclusion-reason')
+    } else {
+        res.redirect('/iteration-1/health-condition-disablity')
+    }
+
+})
+
+router.post('/iteration-1/rfd-answer', function (req, res) {
+
+    var decision = req.session.data['decisionAction']
+
+    if (decision == "Disallowed"){
+        res.redirect('/iteration-1/check-your-answers')
+    } else {
+        res.redirect('/iteration-1/search-bank')
+    }
+
+})
+
+router.post('/iteration-1/bank-answer', function (req, res) {
+
+    var bank = req.session.data['bankAction']
+
+    if (bank == "No"){
+        res.redirect('/iteration-1/search-bank')
+    } else {
+        res.redirect('/iteration-1/payee-details')
+    }
+
+})
+
+
+// discovery concepts
+
+router.post('/discovery-concepts/verify-claimant-details-answer', function (req, res) {
+
+    var verifyAction = req.session.data['verifyAction']
+
+    if (verifyAction == "change"){
+        res.redirect('/discovery-concepts/change-claimant-details')
+    } else if (verifyAction == "close") {
+        res.redirect('/discovery-concepts/close-claim')
+    } else {
+        req.session.data['taskVerifyStatus'] = "complete"
+        res.redirect('/discovery-concepts/claim')
+    }
+
+})
+
+router.post('/discovery-concepts/change-details-answer', function (req, res) {
 
     var changeDetails = req.session.data['changeClaimDetails']
      req.session.data['taskVerifyStatus'] = "inprogress"
 
     if (changeDetails == "name"){
-        res.redirect('/iteration-1/update/name')
+        res.redirect('/discovery-concepts/update/name')
     } else if (changeDetails == "address") {
-        res.redirect('/iteration-1/update/address')
+        res.redirect('/discovery-concepts/update/address')
     } else {
-        res.redirect('/iteration-1/update/phone-number')
+        res.redirect('/discovery-concepts/update/phone-number')
     }
 
 })
 
-router.post('/iteration-1/update/claim-search-postcode', function (req, res) {
+router.post('/discovery-concepts/update/claim-search-postcode', function (req, res) {
 
     req.session.data['claimAddress1'] = "11 street"
     req.session.data['claimAddress2'] = "Heaton"
@@ -46,6 +99,6 @@ router.post('/iteration-1/update/claim-search-postcode', function (req, res) {
     req.session.data['claimAddress4'] = "Tyne Upon Wear"
     req.session.data['claimPostcode'] = "NE6 5DQ"
 
-    res.redirect('/iteration-1/verify-claimant-details')
+    res.redirect('/discovery-concepts/verify-claimant-details')
 
 })
