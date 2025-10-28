b2c961b69257e14fa0aab91e27eaddaf38ec749c
Matthew Barker <matthew.barker3@engineering.digital.dwp.gov.uk>
2025-10-28 08:55:26 +0000

    pull from main
    GitOrigin-RevId: b8242ffc66a30dcea3f061fdd38abba1dec3943b

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,6 +3,20 @@
 // https://prototype-kit.service.gov.uk/docs/adding-css-javascript-and-images
 //
 
+import accessibleAutocomplete from 'accessible-autocomplete'
+
 window.GOVUKPrototypeKit.documentReady(() => {
   // Add JavaScript here
+
+const countries = [
+  'France',
+  'Germany',
+  'United Kingdom'
+]
+
+accessibleAutocomplete({
+  element: document.querySelector('#my-autocomplete-container'),
+  id: 'my-autocomplete', // To match it to the existing <label>.
+  source: countries
+})
 })
